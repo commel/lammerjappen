b68f8aedbf7171c3dac71a0b3a40577d71452f3a
comrad <commel@gmail.com>
2011-12-07 21:14:41 +0100

    Initial Rails and Gems

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lammerjappen::Application.config.secret_token = 'bea062f798c7dd1ecc85675a89230504f4582e0bd1496359c55bcf91fe98a6da24db5d860ced1135c5ba5eb33c0e2759e698e6fc7b45ca91aac0bc56f0dbb358'
